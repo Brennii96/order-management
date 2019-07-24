dc51e319e52a663f3e7e6accdd00c0ef40ef47ef
Brennii96 <brendan.oneill96@hotmail.co.uk>
2019-07-24 20:32:52 +0100

    CSRF protection added with form validation and ability to flash sessions. Input classes used to get fields from forms.

--- classes/Token.php
+++ classes/Token.php
@@ -2,5 +2,24 @@
 
 class Token
 {
-
+    /**
+     * @return mixed
+     */
+    public static function generate()
+    {
+        return Session::put(Config::get('session/token_name'), base64_encode(openssl_random_pseudo_bytes(16)));
+    }
+    /**
+     * @param $token
+     * @return bool
+     */
+    public static function check($token)
+    {
+        $tokenName = Config::get('session/token_name');
+        if (Session::exists($tokenName) && $token === Session::get($tokenName)) {
+            Session::delete($tokenName);
+            return true;
+        }
+        return false;
+    }
 }
