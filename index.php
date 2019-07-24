95e426245c747aee03ae4d3ae1443827bdec28b2
Brennii96 <brendan.oneill96@hotmail.co.uk>
2019-07-24 21:06:14 +0100

    Icons from Flaticon.com

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php
-
 require_once 'core/init.php';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Order Management</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <meta name="apple-mobile-web-app-title" content="Order Management">
+    <meta name="application-name" content="Order Management">
+    <meta name="msapplication-TileColor" content="#00aba9">
+    <meta name="theme-color" content="#ffffff">
+</head>
+<body>
 
-$validate = new Validate();
-
-$validation = $validate->check($_POST, array(
-
-));
+</body>
+</html>
