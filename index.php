0ddb87bdde378733160eb762e0f17b7a4609d941
Brennii96 <brendan.oneill96@hotmail.co.uk>
2019-07-25 20:29:09 +0100

    .

--- index.php
+++ index.php
@@ -5,15 +5,33 @@ require_once 'core/init.php';
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Order Management</title>
     <?php include 'icons.php'; ?>
 </head>
 <body>
     <?php include 'header.php'; ?>
+    <div class="ui container four">
+        <div class="ui column">
+            <div class="ui circular segment">
+                <h2 class="ui header">
+                    <?php echo DB::getInstance()->query("SELECT * FROM products;")->count(); ?> Products Available
+                </h2>
+            </div>
+            <div class="ui circular segment">
+                <h2 class="ui header">
+                    <?php echo DB::getInstance()->query("SELECT * FROM orders")->count(); ?> Orders
+                </h2>
+            </div>
+            <div class="ui circular segment">
+                <h2 class="ui header">
+                    <?php echo DB::getInstance()->query("SELECT * FROM clients;")->count(); ?> Clients
+                </h2>
+            </div>
+        </div>
 
+    </div>
     <?php include 'footer.php'; ?>
 </body>
 </html>
