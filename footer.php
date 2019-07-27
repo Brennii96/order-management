e3c8058e604a2be4f51a25f59166cde8cc7f9220
Brennii96 <brendan.oneill96@hotmail.co.uk>
2019-07-27 19:32:29 +0100

    Drag and drop added needs implementing

--- footer.php
+++ footer.php
@@ -13,4 +13,31 @@
                 id: this.value
             }
         });
+
+    /*
+     * A drag and drop update feature which would be used for updating the order Status. Just drag the order into the dropzone.
+     */
+    // <style href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/themes/base/jquery-ui.css"></style>
+    //<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"/>
+    // $("#draggable-id").val(orderStatus).draggable({
+    //     refreshPositions: true,
+    //     snap: true,
+    //     revert: true,
+    // });
+    // $("#droppable-id").droppable({
+    //     activeClass: "active",
+    //     hoverClass:  "hover",
+    //     drop: function (event, ui) {
+    //         let orderId = ui.draggable.attr('data-id');
+    //         ui.draggable.addClass("hide");
+    //         $.ajax({
+    //             type: "POST",
+    //             url: '/update-status.php',
+    //             dataType: 'json',
+    //             data: {'orderStatus':$("#droppable-id").attr('data')},
+    //         }).done(function( data ) {
+    //
+    //         });
+    //     }
+    // });
 </script>
