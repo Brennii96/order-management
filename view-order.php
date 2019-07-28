db8da55864d1e4b1975e06be9b33febc5cb7be27
Brennii96 <brendan.oneill96@hotmail.co.uk>
2019-07-28 21:26:27 +0100

    Viewing clients orders fixed.

--- view-order.php
+++ view-order.php
@@ -14,7 +14,7 @@ require_once 'core/init.php';
 <?php include_once 'header.php'; ?>
 <div class="ui container">
     <h2><?php echo DB::getInstance()->query("SELECT client_name FROM clients WHERE clients_id = " . Input::get('id') ."")->results()[0]->client_name; ?>'s Orders</h2>
-    <?php if (DB::getInstance()->query("SELECT * FROM orders JOIN products_to_order ON orders.orders_id = products_to_order.orders_id;")->results()[0]->clients_id == Input::get('id')) { ?>
+    <?php if (DB::getInstance()->query("SELECT * FROM products_to_order WHERE clients_id = " . Input::get('id'))->count() == 0) { ?>
         <h3>No orders have been placed.</h3>
     <?php } else { ?>
     <table class="ui large table">
@@ -33,7 +33,7 @@ require_once 'core/init.php';
         </thead>
         <tbody>
         <?php
-        foreach (DB::getInstance()->query("SELECT * FROM orders JOIN products_to_order ON orders.orders_id = products_to_order.orders_id;")->results() as $order) {
+        foreach (DB::getInstance()->query("SELECT * FROM orders JOIN products_to_order ON orders.orders_id = products_to_order.orders_id AND products_to_order.clients_id = ". Input::get('id') .";")->results() as $order) {
             $despatchDate = new DateTime($order->despatch_date);
             $dateTime = new DateTime($order->date_time);
             echo "<tr>
